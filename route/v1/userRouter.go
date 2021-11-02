d063cf0864d67da3ffe917e36069f996c5419d19
Tuuz <silverhawk@foxmail.com>
2021-11-02 17:43:32 +0800

    TuuzMac

--- route/v1/userRouter.go
+++ route/v1/userRouter.go
@@ -10,6 +10,8 @@ func UserRouter(route *gin.RouterGroup) {
 		context.String(0, route.BasePath())
 	})
 
+
 	controller.IndexController(route.Group("index"))
 	controller.InfoController(route.Group("info"))
+	
 }
