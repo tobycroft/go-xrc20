97bba72e99efc29b3bd01e6267cbb153a51a1ef7
Tuuz <silverhawk@foxmail.com>
2021-11-03 15:29:21 +0800

    TuuzMac

--- app/v1/user/controller/info.go
+++ app/v1/user/controller/info.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"main.go/app/v1/user/action/UserInfoAction"
+	"main.go/common/BaseModel/SystemParamModel"
 	"main.go/tuuz/RET"
 )
 
@@ -11,10 +12,15 @@ func InfoController(route *gin.RouterGroup) {
 	route.Use(cors.Default())
 
 	route.Any("get", info_get)
+	route.Any("share", info_share)
 }
 
 func info_get(c *gin.Context) {
 	uid := c.PostForm("uid")
 	user := UserInfoAction.App_userinfo(uid)
 	RET.Success(c, 0, user, nil)
+}
+func info_share(c *gin.Context) {
+	share_url := SystemParamModel.Api_find_val("share_url")
+	RET.Success(c, 0, share_url, nil)
 }
