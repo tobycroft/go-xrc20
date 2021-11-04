affa9830899cbef3b057c8d4ffff85cdf94cd39d
Tuuz <silverhawk@foxmail.com>
2021-11-04 15:14:38 +0800

    TuuzMac

--- app/v1/invest/controller/payment.go
+++ app/v1/invest/controller/payment.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
 )
 
 func PaymentController(route *gin.RouterGroup) {
@@ -11,5 +12,22 @@ func PaymentController(route *gin.RouterGroup) {
 }
 
 func payment_buy(c *gin.Context) {
+	uid := c.PostForm("uid")
+	from, ok := Input.Post("from", c, false)
+	if !ok {
+		return
+	}
+	to, ok := Input.Post("to", c, false)
+	if !ok {
+		return
+	}
+	amount, ok := Input.PostDecimal("amount", c)
+	if !ok {
+		return
+	}
+	mode, ok := Input.PostInt64("mode", c)
+	if !ok {
+		return
+	}
 
 }
