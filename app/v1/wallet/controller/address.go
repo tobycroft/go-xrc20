00bbc13284f4d1ec2dfba0e86199d2efd9ae095a
Tuuz <silverhawk@foxmail.com>
2021-11-05 15:02:02 +0800

    TuuzMac

--- app/v1/wallet/controller/address.go
+++ app/v1/wallet/controller/address.go
@@ -49,30 +49,36 @@ func address_create(c *gin.Context) {
 	var usermodel UserModel.Interface
 	db := tuuz.Db()
 	db.Begin()
-	usermodel.Db = db
-	uid := usermodel.Api_insert(invite_data["id"], address, Calc.Md5(password), "", nil, "cn", address)
-	if uid != 0 {
-		token := Calc.GenerateToken()
-		if !TokenModel.Api_insert(uid, token, "app") {
-			db.Rollback()
-			RET.Fail(c, 401, nil, "token写入失败")
-			return
-		}
-		var useraddress UserAddressModel.Interface
-		useraddress.Db = db
-		if !useraddress.Api_insert(Type, uid, address, "") {
-			db.Rollback()
-			RET.Fail(c, 500, nil, "地址插入失败")
-			return
-		}
-		db.Commit()
-		RET.Success(c, 0, map[string]interface{}{
-			"uid":     uid,
-			"token":   token,
-			"address": address,
-		}, nil)
+	var useraddress UserAddressModel.Interface
+	useraddress.Db = db
+	adr := useraddress.Api_find_address(address)
+	if len(adr) > 0 {
+
 	} else {
-		db.Rollback()
-		RET.Fail(c, 500, nil, "用户创建失败")
+		usermodel.Db = db
+		uid := usermodel.Api_insert(invite_data["id"], address, Calc.Md5(password), "", nil, "cn", address)
+		if uid != 0 {
+			token := Calc.GenerateToken()
+			if !TokenModel.Api_insert(uid, token, "app") {
+				db.Rollback()
+				RET.Fail(c, 401, nil, "token写入失败")
+				return
+			}
+
+			if !useraddress.Api_insert(Type, uid, address, "") {
+				db.Rollback()
+				RET.Fail(c, 500, nil, "地址插入失败")
+				return
+			}
+			db.Commit()
+			RET.Success(c, 0, map[string]interface{}{
+				"uid":     uid,
+				"token":   token,
+				"address": address,
+			}, nil)
+		} else {
+			db.Rollback()
+			RET.Fail(c, 500, nil, "用户创建失败")
+		}
 	}
 }
