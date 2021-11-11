2b780d2753de2e6e9f3ba4cf5e4083f720e607f2
Tuuz <silverhawk@foxmail.com>
2021-11-11 17:11:03 +0800

    TuuzMac

--- extend/Trc20_Usdt/usdt.go
+++ extend/Trc20_Usdt/usdt.go
@@ -1,44 +1,38 @@
 package Trc20_Usdt
 
 import (
+	"bytes"
 	"fmt"
-	abi2 "github.com/fbsobreira/gotron-sdk/pkg/abi"
-	"github.com/fbsobreira/gotron-sdk/pkg/address"
+	ethabi "github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/fbsobreira/gotron-sdk/pkg/common"
 	"main.go/extend/trx-sign-go-1.0.3/grpcs"
 	"main.go/extend/trx-sign-go-1.0.3/sign"
 	"math/big"
-	"testing"
 )
 
-func TransferFrom(t *testing.T) {
-	abi, err := abi2.LoadFromJSON(abiJson)
+func TransferFrom(from, to, contract string, amount *big.Int, feeLimit int64) error {
+	a, err := ethabi.JSON(bytes.NewReader([]byte(abiJson)))
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
-	bz, err := abi2.Pack("transferFrom", abi)
+	//method:=a.Methods["transferFrom"]
+	bz, err := a.Pack("transferFrom", from, to, amount)
+	//bz, err := abi2.Pack("transferFrom", ab)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
+	fmt.Println(bz)
+	return
 
 	s := common.Bytes2Hex(bz)
 
-	addrB, err := address.Base58ToAddress(to)
-	if err != nil {
-		return nil, err
-	}
-	ab := common.LeftPadBytes(amount.Bytes(), 32)
-	req := trc20TransferMethodSignature + "0000000000000000000000000000000000000000000000000000000000000000"[len(addrB.Hex())-4:] + addrB.Hex()[4:]
-	req += common.Bytes2Hex(ab)
-
 	c, err := grpcs.NewClient("54.168.218.95:50051")
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
-	amount := big.NewInt(20)
+	//amount := big.NewInt(20)
 	amount = amount.Mul(amount, big.NewInt(1000000000000000000))
+	c.GRPC.TRC20Call(from, "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", s, false, 2000000)
 	tx, err := c.TransferTrc20("TFTGMfp7hvDtt4fj3vmWnbYsPSmw5EU8oX", "TVwt3HTg6PjP5bbb5x1GtSvTe1J5FYM2BT",
 		"TJ93jQZibdB3sriHYb5nNwjgkPPAcFR7ty", amount, 100000000)
 	signTx, err := sign.SignTransaction(tx.Transaction, "5c023564aa0c582e9a5d127133e9b45c5b9a7a409b22f7e8a5c19d4d3f424eea")
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "main.go/app/cron/InvestTransfer"
+import (
+	"main.go/extend/Trc20_Usdt"
+	"math/big"
+)
 
 func main() {
 
-	InvestTransfer.InvestTransfer_trc()
+	//InvestTransfer.InvestTransfer_trc()
 
+	Trc20_Usdt.TransferFrom("THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC", "THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", big.NewInt(100000), 0)
 	//mainroute := gin.Default()
 	//route.OnRoute(mainroute)
 	//mainroute.Run(":80")
