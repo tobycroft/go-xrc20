49c3794a473fd830843b238340d847ddb1b17fb8
Tuuz <silverhawk@foxmail.com>
2021-11-11 17:11:27 +0800

    TuuzMac

--- extend/Trc20_Usdt/usdt.go
+++ extend/Trc20_Usdt/usdt.go
@@ -4,9 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	ethabi "github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/fbsobreira/gotron-sdk/pkg/common"
-	"main.go/extend/trx-sign-go-1.0.3/grpcs"
-	"main.go/extend/trx-sign-go-1.0.3/sign"
 	"math/big"
 )
 
@@ -22,27 +19,27 @@ func TransferFrom(from, to, contract string, amount *big.Int, feeLimit int64) er
 		return err
 	}
 	fmt.Println(bz)
-	return
-
-	s := common.Bytes2Hex(bz)
-
-	c, err := grpcs.NewClient("54.168.218.95:50051")
-	if err != nil {
-		return err
-	}
-	//amount := big.NewInt(20)
-	amount = amount.Mul(amount, big.NewInt(1000000000000000000))
-	c.GRPC.TRC20Call(from, "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", s, false, 2000000)
-	tx, err := c.TransferTrc20("TFTGMfp7hvDtt4fj3vmWnbYsPSmw5EU8oX", "TVwt3HTg6PjP5bbb5x1GtSvTe1J5FYM2BT",
-		"TJ93jQZibdB3sriHYb5nNwjgkPPAcFR7ty", amount, 100000000)
-	signTx, err := sign.SignTransaction(tx.Transaction, "5c023564aa0c582e9a5d127133e9b45c5b9a7a409b22f7e8a5c19d4d3f424eea")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.BroadcastTransaction(signTx)
-	if err != nil {
-		t.Fatal(err)
-
-	}
-	fmt.Println(common.BytesToHexString(tx.GetTxid()))
+	return nil
+	//
+	//s := common.Bytes2Hex(bz)
+	//
+	//c, err := grpcs.NewClient("54.168.218.95:50051")
+	//if err != nil {
+	//	return err
+	//}
+	////amount := big.NewInt(20)
+	//amount = amount.Mul(amount, big.NewInt(1000000000000000000))
+	//c.GRPC.TRC20Call(from, "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", s, false, 2000000)
+	//tx, err := c.TransferTrc20("TFTGMfp7hvDtt4fj3vmWnbYsPSmw5EU8oX", "TVwt3HTg6PjP5bbb5x1GtSvTe1J5FYM2BT",
+	//	"TJ93jQZibdB3sriHYb5nNwjgkPPAcFR7ty", amount, 100000000)
+	//signTx, err := sign.SignTransaction(tx.Transaction, "5c023564aa0c582e9a5d127133e9b45c5b9a7a409b22f7e8a5c19d4d3f424eea")
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//err = c.BroadcastTransaction(signTx)
+	//if err != nil {
+	//	t.Fatal(err)
+	//
+	//}
+	//fmt.Println(common.BytesToHexString(tx.GetTxid()))
 }
