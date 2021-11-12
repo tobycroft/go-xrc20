e871afc7e0e1b3c154f279b8c77dbbf2775c61d4
Tuuz <silverhawk@foxmail.com>
2021-11-12 15:20:32 +0800

    TuuzMac

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"main.go/extend/Trc20_Usdt"
-	"math/big"
+	"github.com/gin-gonic/gin"
+	"main.go/route"
 )
 
 func main() {
 
-	//InvestTransfer.InvestTransfer_trc()
-t:=Trc20_Usdt.InitTranns("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t")
-t.TransferFrom()
-	Trc20_Usdt.TransferFrom("TBA6CypYJizwA9XdC7Ubgc5F1bxrQ7SqPt", "TLFtevwBqEV9fkC4nNwmTRtFiygPJKpqci", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", big.NewInt(22884), 0)
-	//mainroute := gin.Default()
-	//route.OnRoute(mainroute)
-	//mainroute.Run(":80")
+	mainroute := gin.Default()
+	route.OnRoute(mainroute)
+	mainroute.Run(":80")
 
 }
